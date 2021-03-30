1ee3d51eb65958dfb1f61ded365c2cad0d6ecc30
mits0s2019 <mits0s200efta@gmail.com>
2021-03-30 13:46:48 +0300

    ViewChildren example, home - car components

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,12 @@
-import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  OnInit,
+  QueryList,
+  ViewChild,
+  ViewChildren,
+} from '@angular/core';
+import { CarComponent } from '../car/car.component';
 import { Car } from '../interfaces/car';
 import { CarService } from '../services/car.service';
 
@@ -7,13 +15,28 @@ import { CarService } from '../services/car.service';
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss'],
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   cars: Car[];
+
+  @ViewChildren(CarComponent) carElements: QueryList<CarComponent>;
 
   constructor(private carService: CarService) {}
 
   ngOnInit(): void {
     console.log('HomeComponent initialized');
     this.carService.getCars().subscribe((data) => (this.cars = data));
+  }
+
+  ngAfterViewInit() {
+    this.carElements.changes.subscribe(() => {
+      // If clients is set from an async call ( getCars() ) then the elements don't exist in ngAfterViewInit() yet.
+      this.carElements.toArray().forEach((carElement) => {
+        setInterval(() => {
+          carElement.inCart
+            ? carElement.car.price
+            : (carElement.car.price = Math.random() * 100000 + 10000);
+        }, 1000);
+      });
+    });
   }
 }
